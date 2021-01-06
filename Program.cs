8c8a0201548b03e62c045fed1c7a99c5bd199a8f
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-01-06 15:57:09 -0300

    Implemented file reading and added new task Launch

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        var textA = File.ReadAllText("Texts/A.txt");
+        Console.WriteLine($"Text A: \n{textA}\n");
+
+        var textB = File.ReadAllText("Texts/B.txt");
+        Console.WriteLine($"Text B: \n{textB}");
     }
 }
