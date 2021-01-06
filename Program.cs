3448f89d4a35ea42b710b6ce9507cb79b732dd40
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-01-06 16:19:25 -0300

    Added Klingon Preposition Count Function

--- Program.cs
+++ Program.cs
@@ -6,9 +6,25 @@ class Program
     static void Main(string[] args)
     {
         var textA = File.ReadAllText("Texts/A.txt");
-        Console.WriteLine($"Text A: \n{textA}\n");
+        Console.WriteLine($"Text A preposition ammount: {getPrepositionAmmount(textA)}");
 
         var textB = File.ReadAllText("Texts/B.txt");
-        Console.WriteLine($"Text B: \n{textB}");
+        Console.WriteLine($"Text B preposition ammount: {getPrepositionAmmount(textB)}");
+    }
+
+    static int getPrepositionAmmount(string text) {
+        var words = text.Split(" ");
+        int prepositionAmmount = 0;
+
+        foreach (var word in words) {
+            var isLength3 = word.Length == 3;
+            var doesntHaveD = !word.Contains('d');
+            var isLastLetterTypeBar = word[2] != 's' && word[2] != 'l' && word[2] != 'f' && word[2] != 'w' && word[2] != 'k';
+
+            if (isLength3 && doesntHaveD && isLastLetterTypeBar)
+                prepositionAmmount++;
+        }
+
+        return prepositionAmmount;
     }
 }
