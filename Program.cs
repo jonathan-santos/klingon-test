e59c2f8d5928e4508dc6e6d22d869a6fba1cd162
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-01-06 16:30:11 -0300

    Added Klingon Verb Count Function, implemented verbCounting in texts A and B and created function to tell if letter is a Foo

--- Program.cs
+++ Program.cs
@@ -7,9 +7,17 @@ class Program
     {
         var textA = File.ReadAllText("Texts/A.txt");
         Console.WriteLine($"Text A preposition ammount: {getPrepositionAmmount(textA)}");
+        Console.WriteLine($"Text A verb ammount: {getVerbAmmount(textA)}");
+        Console.WriteLine($"Text A verb ammount (first person): {getVerbAmmount(textA, firstPersonOnly: true)}");
 
         var textB = File.ReadAllText("Texts/B.txt");
-        Console.WriteLine($"Text B preposition ammount: {getPrepositionAmmount(textB)}");
+        Console.WriteLine($"\nText B preposition ammount: {getPrepositionAmmount(textB)}");
+        Console.WriteLine($"Text B verb ammount: {getVerbAmmount(textB)}");
+        Console.WriteLine($"Text B verb ammount (first person): {getVerbAmmount(textB, firstPersonOnly: true)}");
+    }
+
+    static bool isLetterFoo(char letter) {
+        return letter == 's' || letter == 'l' || letter == 'f' || letter == 'w' || letter == 'k';
     }
 
     static int getPrepositionAmmount(string text) {
@@ -19,12 +27,30 @@ class Program
         foreach (var word in words) {
             var isLength3 = word.Length == 3;
             var doesntHaveD = !word.Contains('d');
-            var isLastLetterTypeBar = word[2] != 's' && word[2] != 'l' && word[2] != 'f' && word[2] != 'w' && word[2] != 'k';
+            var isLastLetterTypeBar = !isLetterFoo(word[2]);
 
             if (isLength3 && doesntHaveD && isLastLetterTypeBar)
                 prepositionAmmount++;
         }
 
         return prepositionAmmount;
+    }
+
+    static int getVerbAmmount(string text, bool firstPersonOnly = false) {
+        var words = text.Split(" ");
+        int verbAmmount = 0;
+
+        foreach (var word in words) {
+            var isLengthEqualOrGreaterThan8 = word.Length >= 8;
+            var isLastLetterTypeFoo = isLetterFoo(word[word.Length - 1]);
+
+            if (firstPersonOnly && isLetterFoo(word[0]))
+                continue;
+
+            if (isLengthEqualOrGreaterThan8 && isLastLetterTypeFoo)
+                verbAmmount++;
+        }
+
+        return verbAmmount;
     }
 }
