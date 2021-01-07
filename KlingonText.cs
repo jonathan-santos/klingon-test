d95e762c7f0dcb1811dd093b0fc5356c73de3c83
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-01-07 11:38:55 -0300

    Created function GetDistinctNumbersInText and ConvertWordToNumber, static member _letterNumberValues and property BeautifulDistinctNumbersInText in class KlingonText, added new print in Main function to use the new property in class KlingonText

--- KlingonText.cs
+++ KlingonText.cs
@@ -1,9 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 public class KlingonText
 {
+    static Dictionary<char, int> _letterNumberValues = new Dictionary<char, int> ()
+    {
+        ['k'] = 0,
+        ['b'] = 1,
+        ['w'] = 2,
+        ['r'] = 3,
+        ['q'] = 4,
+        ['d'] = 5,
+        ['n'] = 6,
+        ['f'] = 7,
+        ['x'] = 8,
+        ['j'] = 9,
+        ['m'] = 10,
+        ['l'] = 11,
+        ['v'] = 12,
+        ['h'] = 13,
+        ['t'] = 14,
+        ['c'] = 15,
+        ['g'] = 16,
+        ['z'] = 17,
+        ['p'] = 18,
+        ['s'] = 19
+    };
+    
     public string Text { get; set; }
     public int PrepositionCount { get; set; }
     public int VerbCount { get; set; }
     public int VerbInFirstPersonCount { get; set; }
+    public ulong[] BeautifulDistinctNumbersInText { get; set; }
 
     public KlingonText(string text)
     {
@@ -11,6 +40,7 @@ public class KlingonText
         PrepositionCount = GetPrepositionCount();
         VerbCount = GetVerbCount();
         VerbInFirstPersonCount = GetVerbCount(firstPersonOnly: true);
+        BeautifulDistinctNumbersInText = GetDistinctNumbersInText();
     }
 
     bool isLetterFoo(char letter)
@@ -52,5 +82,31 @@ public class KlingonText
         }
 
         return verbCount;
+    }
+
+    ulong[] GetDistinctNumbersInText()
+    {
+        var words = Text.Split(" ");
+        ulong[] numbers = new ulong[words.Length];
+
+        for (var i = 0; i < words.Length; i++)
+            numbers[i] = ConvertWordToNumber(words[i]);
+        
+        return numbers.Where(n => n >= 440566 && n % 3 == 0)
+            .Distinct<ulong>()
+            .ToArray();;
+    }
+
+    ulong ConvertWordToNumber(string word)
+    {
+        ulong value = 0;
+
+        for (var i = 0; i < word.Length; i++)
+        {
+            var add = _letterNumberValues[word[i]] * Math.Pow(20, i); 
+            value += (ulong) add;
+        }
+
+        return value;
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,10 +9,12 @@ class Program
         Console.WriteLine($"Text A preposition ammount: {textA.PrepositionCount}");
         Console.WriteLine($"Text A verb ammount: {textA.VerbCount}");
         Console.WriteLine($"Text A verb ammount (first person): {textA.VerbInFirstPersonCount}");
+        Console.WriteLine($"Text A beautiful distinct numbers quantity: {textA.BeautifulDistinctNumbersInText.Length}");
 
         var textB = new KlingonText(File.ReadAllText("Texts/B.txt"));
         Console.WriteLine($"\nText B preposition ammount: {textB.PrepositionCount}");
         Console.WriteLine($"Text B verb ammount: {textB.VerbCount}");
         Console.WriteLine($"Text B verb ammount (first person): {textB.VerbInFirstPersonCount}");
+        Console.WriteLine($"Text B beautiful distinct numbers quantity: {textB.BeautifulDistinctNumbersInText.Length}");
     }
 }
